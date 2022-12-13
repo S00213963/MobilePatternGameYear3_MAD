909a5e7448376bd4e19f73f273c3ee00251dd46e
StephenMeehan1996 <78025435+StephenMeehan1996@users.noreply.github.com>
2022-12-13 16:35:22 +0000

    initial project setup

--- app/src/main/java/com/app/mobilegameapp/MainActivity.java
+++ app/src/main/java/com/app/mobilegameapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.app.mobilegameapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+    public void GameStart(View V)
+    {
+        Intent i = (new Intent(MainActivity.this, Game.class));
+//        i.putExtra("distance", disText);
+//        i.putExtra("calories", calText);
+        startActivity(i);
+
+
+
+    }
+
+
 }
