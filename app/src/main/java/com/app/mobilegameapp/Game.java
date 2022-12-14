ae06d70ad738b6d3138bc436be6408cadc0cfef3
StephenMeehan1996 <78025435+StephenMeehan1996@users.noreply.github.com>
2022-12-14 11:37:26 +0000

    basic gameplay implemented

--- app/src/main/java/com/app/mobilegameapp/Game.java
+++ app/src/main/java/com/app/mobilegameapp/Game.java
@@ -7,7 +7,15 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Game extends AppCompatActivity implements SensorEventListener {
 
@@ -28,20 +36,83 @@ public class Game extends AppCompatActivity implements SensorEventListener {
     boolean highLimitD = false;
     boolean highLimitL = false;
     boolean highLimitR = false;
+
+    boolean gameStarted = false;
     int XUpCount = 0;
     int XDownCount = 0;
     int lCount = 0;
     int rCount = 0;
 
-    TextView tvx, tvy, tvz, tvUp, tvLeft, tvRight, tvDown;
+    //Game varibles //
+    int roundCounter = 1;
+    int guessCount = 0;
+    int roundTime = 6000;
+
+
+    TextView tvx, tvy, tvz, tvUp, tvLeft, tvRight, tvDown, gameText, tvRound;
     private SensorManager mSensorManager;
     private Sensor mSensor;
+
+    //buttons
+    private final int RED = 1;
+    private final int BLUE = 2;
+
+    private final int YELLOW = 3;
+    private final int GREEN = 4;
+
+    Animation anim;
+
+    StringBuilder sb;
+    Button bRed, bBlue, bYellow, bGreen;
+    TextView tv;
+
+    int sequenceCount = 4, n = 0;
+    int[] gameSequence = new int[120];
+    int arrayIndex = 0;
+
+    public void test(int num1, int num2)
+    {
+        CountDownTimer ct = new CountDownTimer(num1,  num2) {
+
+            public void onTick(long millisUntilFinished) {
+                //mTextField.setText("seconds remaining: " + millisUntilFinished / 1500);
+                oneButton(0);
+                //here you can have your logic to set text to edittext
+            }
+
+            public void onFinish() {
+                //mTextField.setText("done!");
+                // we now have the game sequence
+
+                gameText.setText(sb);
+                for (int i = 0; i< arrayIndex; i++)
+                    Log.d("game sequence", String.valueOf(gameSequence[i]));
+                // start next activity
+                gameStarted = true;
+                // put the sequence into the next activity
+                // stack overglow https://stackoverflow.com/questions/3848148/sending-arrays-with-intent-putextra
+                //Intent i = new Intent(A.this, B.class);
+                //i.putExtra("numbers", array);
+                //startActivity(i);
+
+                // start the next activity
+                // int[] arrayB = extras.getIntArray("numbers");
+            }
+        };
+
+        ct.start();
+
+
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
 
+        setContentView(R.layout.activity_game);
+        getSupportActionBar().hide();
 
 
         tvx = findViewById(R.id.tvX);
@@ -52,9 +123,131 @@ public class Game extends AppCompatActivity implements SensorEventListener {
         tvLeft = findViewById(R.id.left);
         tvRight = findViewById(R.id.right);
 
+        bRed = findViewById(R.id.btnRed);
+        bBlue = findViewById(R.id.btnBlue);
+        bYellow = findViewById(R.id.btnYellow);
+        bGreen = findViewById(R.id.btnGreen);
+
+        gameText = findViewById(R.id.tvGame);
+        tvRound = findViewById(R.id.tvRound);
+
+
+
         // we are going to use the sensor service
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+
+    }
+
+    private void oneButton(int num) {
+
+        if (gameStarted == false)
+        {
+            n = getRandom(sequenceCount);
+            sb.append(String.valueOf(n) + ", ");
+         //   Toast.makeText(this, "Number = " + n, Toast.LENGTH_SHORT).show();
+
+            switch (n) {
+                case 1:
+                    flashButton(bRed);
+                    gameSequence[arrayIndex++] = BLUE;
+                    break;
+                case 2:
+                    flashButton(bBlue);
+                    gameSequence[arrayIndex++] = RED;
+                    break;
+                case 3:
+                    flashButton(bYellow);
+                    gameSequence[arrayIndex++] = YELLOW;
+                    break;
+                case 4:
+                    flashButton(bGreen);
+                    gameSequence[arrayIndex++] = GREEN;
+                    break;
+                default:
+                    break;
+            }   // end switch
+
+        }
+        if(gameStarted == true && guessCount <= sequenceCount)
+        {
+
+            switch (num) {
+                case 1:
+                    flashButton(bRed);
+                    guessCount++;
+                   // gameSequence[arrayIndex++] = BLUE;
+                    break;
+                case 2:
+                    flashButton(bBlue);
+                    guessCount++;
+                  //  gameSequence[arrayIndex++] = RED;
+                    break;
+                case 3:
+                    flashButton(bYellow);
+                    guessCount++;
+                  //  gameSequence[arrayIndex++] = YELLOW;
+                    break;
+                case 4:
+                    flashButton(bGreen);
+                    guessCount++;
+                  //  gameSequence[arrayIndex++] = GREEN;
+                    break;
+                default:
+                    break;
+            }
+
+            tvRound.setText(String.valueOf(guessCount));
+        }
+        else
+           {
+               tvRound.setText("Game Over");
+              // nextRound();
+
+
+           }
+
+    }
+
+    private void flashButton(Button button) {
+
+        anim = new AlphaAnimation(1,0);
+        anim.setDuration(1000);
+
+         if (gameStarted)
+         {
+             anim.setDuration(300);
+
+         }
+         else
+         {
+             anim.setDuration(1000);
+         }
+
+        anim.setRepeatCount(0);
+        button.startAnimation(anim);
+
+    }
+
+    //
+    // return a number between 1 and maxValue
+    private int getRandom(int maxValue) {
+
+        return ((int) ((Math.random() * maxValue) + 1));
+    }
+
+    public void doPlay(View view) {
+        sb = new StringBuilder("Result : ");
+     //   ct.start();
+        test(6000, 1500);
+       // ct.set
+    }
+
+    public void nextRound() {
+        roundTime += 1500;
+        test(roundTime, 1500);
+
     }
 
 
@@ -77,59 +270,71 @@ public class Game extends AppCompatActivity implements SensorEventListener {
     }
 
 
+
+
+
     @Override
     public void onSensorChanged(SensorEvent event) {
 
-        float x = event.values[0];
-        float y = event.values[1];
-        float z = event.values[2];
+        if(gameStarted == true)
+        {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
 
-        tvx.setText(String.valueOf(x));
-        tvy.setText(String.valueOf(y));
-        tvz.setText(String.valueOf(z));
+            tvx.setText(String.valueOf(x));
+            tvy.setText(String.valueOf(y));
+            tvz.setText(String.valueOf(z));
 
-        if ((x > XUp) && (highLimitU == false)) {
-            highLimitU = true;
-        }
-        if ((x < XUpB) && (highLimitU == true)) {
-            // we have a tilt to the north
-            XUpCount++;
-            tvUp.setText(String.valueOf(XUpCount));
-            highLimitU = false;
-        }
+            if ((x > XUp) && (highLimitU == false)) {
+                highLimitU = true;
+            }
+            if ((x < XUpB) && (highLimitU == true)) {
+                // we have a tilt to the north
+                XUpCount++;
+                tvUp.setText(String.valueOf(XUpCount));
+                oneButton(1);
+                highLimitU = false;
+            }
 
-        if ((x < XDown) && (highLimitD == false)) {
-            highLimitD = true;
-        }
-        if ((x > XDownB) && (highLimitD == true)) {
-            // we have a tilt to the north
-            XDownCount++;
-            tvDown.setText(String.valueOf(XDownCount));
-            highLimitD = false;
-        }
-
-
-        if ((y < YLeft) && (highLimitL == false)) {
-            highLimitL = true;
-        }
-        if ((y > YLeftB) && (highLimitL == true)) {
-            // we have a tilt to the north
-            lCount++;
-            tvLeft.setText(String.valueOf(lCount));
-            highLimitL = false;
-        }
+            if ((x < XDown) && (highLimitD == false)) {
+                highLimitD = true;
+            }
+            if ((x > XDownB) && (highLimitD == true)) {
+                // we have a tilt to the north
+                XDownCount++;
+                tvDown.setText(String.valueOf(XDownCount));
+                oneButton(3);
+                highLimitD = false;
+            }
 
 
+            if ((y < YLeft) && (highLimitL == false)) {
+                highLimitL = true;
+            }
+            if ((y > YLeftB) && (highLimitL == true)) {
+                // we have a tilt to the north
+                lCount++;
+                tvLeft.setText(String.valueOf(lCount));
+                oneButton(4);
+                highLimitL = false;
+            }
 
-        if ((y > YRight) && (highLimitR == false)) {
-            highLimitR = true;
+
+
+            if ((y > YRight) && (highLimitR == false)) {
+                highLimitR = true;
+            }
+            if ((y < YRightB) && (highLimitR == true)) {
+                // we have a tilt to the north
+                rCount++;
+                tvRight.setText(String.valueOf(rCount));
+                oneButton(2);
+                highLimitR = false;
+            }
+
         }
-        if ((y < YRightB) && (highLimitR == true)) {
-            // we have a tilt to the north
-            rCount++;
-            tvRight.setText(String.valueOf(rCount));
-            highLimitR = false;
-        }
+
 
 
     }
--- app/src/main/java/com/app/mobilegameapp/MainActivity.java
+++ app/src/main/java/com/app/mobilegameapp/MainActivity.java
@@ -2,29 +2,147 @@ package com.app.mobilegameapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private long lastUpdate;
+
+    AnimatedView animatedView = null;
+    ShapeDrawable mDrawable = new ShapeDrawable();
+    public static int x;
+    public static int y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        lastUpdate = System.currentTimeMillis();
+
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer,
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.activity_main, menu);
+//        return true;
+//    }
+
+    @Override
+    public void onAccuracyChanged(Sensor arg0, int arg1) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // TODO Auto-generated method stub
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+            x -=  (int) event.values[0] * 3;
+            y += (int) event.values[1] * 3;
+
+        }
+    }
+
+    public class AnimatedView extends androidx.appcompat.widget.AppCompatImageView {
+
+        static final int width = 150;
+        static final int height = 150;
+
+        public AnimatedView(Context context) {
+            super(context);
+            // TODO Auto-generated constructor stub
+
+            mDrawable = new ShapeDrawable(new OvalShape());
+            mDrawable.getPaint().setColor(0xffffAC23);
+            mDrawable.setBounds(x, y, x + width, y + height);
+
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+
+            mDrawable.setBounds(x, y, x + width, y + height);
+            mDrawable.draw(canvas);
+            invalidate();
+        }
+    }
+
     public void GameStart(View V)
     {
         Intent i = (new Intent(MainActivity.this, Game.class));
 //        i.putExtra("distance", disText);
 //        i.putExtra("calories", calText);
         startActivity(i);
-
+//        animatedView = new AnimatedView(this);
+//        setContentView(animatedView);
 
 
     }
 
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
